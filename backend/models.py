dbc39dac85c610e7d0b28a5e2e5952270253a4c2
israellopezdeveloper <israel.lopez.developer@gmail.com>
2025-07-23 11:02:35 +0400

    create backend functionality

--- backend/models.py
+++ backend/models.py
@@ -4,10 +4,6 @@ from sqlmodel import Field, SQLModel
 
 
 class UploadedFile(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    filename: str
-    filepath: str
-    wallet_address: str
-    created_at: datetime = Field(
-        default_factory=lambda: datetime.now(UTC),
-    )
+    filename: str = Field(primary_key=True)
+    wallet_address: str = Field(primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
